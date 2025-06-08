745b800afab5ebdf2c1e96bbf1a8f7d6cc07f880
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-06-08 13:09:25 +0300

    Add main application structure and home screen layout

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/View/home.dart';
+
+void main(){
+
+
+  runApp(MyWidget());
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: Homescrren (
+        
+      ),
+    );
+  }
+}
